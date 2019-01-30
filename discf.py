0d6cc195b98756a4afb2edd1b6e05ecbe4ba7c30
Charles Li <lixianzhe0112@gmail.com>
2019-01-30 12:17:01 +0000

    31-12-2018 result update

--- discf.py
+++ discf.py
@@ -14,8 +14,8 @@ else:
 #===============================================Inputs=========================================================
 dcf_period = 9
 perpetuity_growth_rate = 0.0375  # Gordon growth rate - long term growth rate
-no_of_shares = 474429699 # 474965441
-net_debt_cash = 125300000    
+no_of_shares = 474318625 
+net_debt_cash = 126200000    
 discount_rate = 0.0825 # WACC - cost of equity
 #=============================================================================================================
 def disc_cash_flow(dic_data, input_dic, now=False, fractional=False, dcf_p=dcf_period, disc_rate = discount_rate, dcf_start=dcf_start_year):
